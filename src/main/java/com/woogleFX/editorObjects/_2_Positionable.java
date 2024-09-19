cd680fb65bff3b489abba96b8718a74b92b602c9
Darxoon <darxoon@outlook.com>
2024-09-19 22:14:40 +0200

    Make Positionable class support any attribute names

--- src/main/java/com/woogleFX/editorObjects/_2_Positionable.java
+++ src/main/java/com/woogleFX/editorObjects/_2_Positionable.java
@@ -1,5 +1,7 @@
 package com.woogleFX.editorObjects;
 
+import java.util.Arrays;
+
 import com.woogleFX.editorObjects.attributes.AttributeAdapter;
 import com.woogleFX.editorObjects.attributes.dataTypes.Position;
 import com.woogleFX.editorObjects.objectCreators.ObjectCreator;
@@ -8,30 +10,44 @@ import com.worldOfGoo2.misc._2_Point;
 
 public abstract class _2_Positionable extends EditorObject {
 
-    public _2_Positionable(EditorObject parent, String type, GameVersion version) {
+    private String[] pointAttributes;
+    
+    public _2_Positionable(EditorObject parent, String type, GameVersion version, String... pointAttributes) {
         super(parent, type, version);
         
-        addAttributeAdapter("pos", AttributeAdapter.pointAttributeAdapter(this, "pos", "pos"));
+        if (pointAttributes.length == 0) pointAttributes = new String[] { "pos" };
+        this.pointAttributes = pointAttributes;
+        
+        for (String pointName : pointAttributes) {
+            addAttributeAdapter(pointName, AttributeAdapter.pointAttributeAdapter(this, pointName, pointName));
+        }
     }
     
     @Override
     public void onLoaded() {
         super.onLoaded();
         
-        EditorObject pos = getChild("pos");
-        setAttribute2("pos", pos.getAttribute("x").stringValue() +
-                "," + pos.getAttribute("y").stringValue());
-        pos.getAttribute("x").addChangeListener((observable, oldValue, newValue) ->
-                setAttribute2("pos", newValue + "," + getAttribute2("pos").positionValue().getY()));
-        pos.getAttribute("y").addChangeListener((observable, oldValue, newValue) ->
-                setAttribute2("pos", getAttribute2("pos").positionValue().getX() + "," + newValue));
+        for (String pointName : pointAttributes) {
+            EditorObject point = getChild(pointName);
+            
+            if (point != null) {
+                setAttribute2(pointName, point.getAttribute("x").stringValue() +
+                        "," + point.getAttribute("y").stringValue());
+                point.getAttribute("x").addChangeListener((observable, oldValue, newValue) ->
+                        setAttribute2(pointName, newValue + "," + getAttribute2(pointName).positionValue().getY()));
+                point.getAttribute("y").addChangeListener((observable, oldValue, newValue) ->
+                        setAttribute2(pointName, getAttribute2(pointName).positionValue().getX() + "," + newValue));
+            }
+        }
     }
     
     public void createPosition() {
-        EditorObject pos = ObjectCreator.create2(_2_Point.class, this, GameVersion.VERSION_WOG2);
-        pos.setAttribute("x", 0);
-        pos.setAttribute("y", 0);
-        pos.setTypeID("pos");
+        for (String pointName : pointAttributes) {
+            EditorObject pos = ObjectCreator.create2(_2_Point.class, this, GameVersion.VERSION_WOG2);
+            pos.setAttribute("x", 0);
+            pos.setAttribute("y", 0);
+            pos.setTypeID(pointName);
+        }
     }
 
     public Position getPosition() {
